fdc4bbc767e3591c542129bbde00d9a3e98a62a0
papiricoh <slendeman0200@gmail.com>
2023-09-19 10:18:24 +0200

    added offers and check altars

--- src/main/java/org/papiricoh/townyreligion/object/Religion.java
+++ src/main/java/org/papiricoh/townyreligion/object/Religion.java
@@ -1,6 +1,12 @@
 package org.papiricoh.townyreligion.object;
 
 import com.palmergames.bukkit.towny.object.Town;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.papiricoh.townyreligion.object.god.God;
 
 import java.util.ArrayList;
@@ -14,9 +20,11 @@ public class Religion {
     private ArrayList<Religion> allies;
     private ArrayList<Religion> enemies;
     private God main_god;
+    private Chest altar;
+    private boolean active_boost = false;
 
 
-    public Religion(String name, Town foundingTown, Date timeEstablished, ArrayList<Town> towns, ArrayList<Religion> allies, ArrayList<Religion> enemies, God mainGod) {
+    public Religion(String name, Town foundingTown, Date timeEstablished, ArrayList<Town> towns, ArrayList<Religion> allies, ArrayList<Religion> enemies, God mainGod, Block altar) {
         this.name = name;
         this.founding_town = foundingTown;
         this.time_established = timeEstablished;
@@ -24,7 +32,9 @@ public class Religion {
         this.allies = allies;
         this.enemies = enemies;
         this.main_god = mainGod;
+        this.altar = this.configAltarBlock(altar);
     }
+
 
     public Religion(String name, Town founding_town, God god) {
         this.name = name;
@@ -34,6 +44,32 @@ public class Religion {
         this.allies = new ArrayList<>();
         this.enemies = new ArrayList<>();
         this.main_god = god;
+        this.altar = null;
+    }
+
+    private Chest configAltarBlock(Block altar) {
+        Block block = new Location(altar.getWorld(), altar.getX(), altar.getY(), altar.getZ()).getBlock();
+
+        if(block.getType().equals(Material.CHEST)) {
+            Chest chest = (Chest) block;
+            return chest;
+        }
+        return null;
+    }
+
+    public boolean offerToGod() {
+        if(this.altar == null || this.main_god == null) {
+            this.active_boost = false;
+            return false;
+        }
+        Inventory altar_inventory = this.altar.getBlockInventory();
+        if(altar_inventory.contains(this.main_god.getMaterial()) || altar_inventory.getItem(altar_inventory.first(main_god.getMaterial())).getAmount() >= 4) {
+            altar_inventory.remove(new ItemStack(main_god.getMaterial(), 4));
+            this.active_boost = true;
+            return true;
+        }
+        this.active_boost = false;
+        return false;
     }
 
     public boolean containsTown(Town town) {
--- src/main/java/org/papiricoh/townyreligion/object/god/God.java
+++ src/main/java/org/papiricoh/townyreligion/object/god/God.java
@@ -1,21 +1,24 @@
 package org.papiricoh.townyreligion.object.god;
 
+import org.bukkit.Material;
 import org.bukkit.potion.PotionEffectType;
 
 public class God {
     private String name;
     private String lore;
     private final PotionEffectType potion_effect_type;
+    private Material material;
 
     public God(String name, PotionEffectType potion_effect_type) {
         this.name = name;
         this.potion_effect_type = potion_effect_type;
     }
 
-    public God(String name, String lore, PotionEffectType potion_effect_type) {
+    public God(String name, String lore, PotionEffectType potion_effect_type, Material material) {
         this.name = name;
         this.lore = lore;
         this.potion_effect_type = potion_effect_type;
+        this.material = material;
     }
 
     public void setLore(String lore) {
@@ -32,5 +35,9 @@ public class God {
 
     public PotionEffectType getPotion_effect_type() {
         return potion_effect_type;
+    }
+
+    public Material getMaterial() {
+        return this.material;
     }
 }
