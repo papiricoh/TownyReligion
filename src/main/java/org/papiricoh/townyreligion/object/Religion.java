5ed43f853f178a1816ff04e309076394f9093603
papiricoh <slendeman0200@gmail.com>
2023-09-19 14:01:30 +0200

    Added sacredBook

--- src/main/java/org/papiricoh/townyreligion/object/Religion.java
+++ src/main/java/org/papiricoh/townyreligion/object/Religion.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.papiricoh.townyreligion.object.book.SacredBook;
 import org.papiricoh.townyreligion.object.god.God;
 
 import java.util.ArrayList;
@@ -17,35 +18,36 @@ public class Religion {
     private Town founding_town;
     private Date time_established;
     private ArrayList<Town> towns;
-    private ArrayList<Religion> allies;
-    private ArrayList<Religion> enemies;
+    private Religion father_religion;
     private God main_god;
     private Chest altar;
     private boolean active_boost = false;
 
+    private SacredBook sacredBook;
 
-    public Religion(String name, Town foundingTown, Date timeEstablished, ArrayList<Town> towns, ArrayList<Religion> allies, ArrayList<Religion> enemies, God mainGod, Block altar) {
+
+    public Religion(String name, Town foundingTown, Date timeEstablished, ArrayList<Town> towns, Religion father_religion, God mainGod, Block altar, SacredBook sacredBook) {
         this.name = name;
         this.founding_town = foundingTown;
         this.time_established = timeEstablished;
         this.towns = towns;
-        this.allies = allies;
-        this.enemies = enemies;
+        this.father_religion = father_religion;
         this.main_god = mainGod;
         this.altar = this.configAltarBlock(altar);
+        this.sacredBook = sacredBook;
     }
 
 
-    public Religion(String name, Town founding_town, God god) {
+    public Religion(String name, Town founding_town, Religion father_religion, God god) {
         this.name = name;
         this.founding_town = founding_town;
         this.time_established = new Date();
-        this.towns = new ArrayList<>();
-        this.allies = new ArrayList<>();
-        this.enemies = new ArrayList<>();
+        this.father_religion = father_religion;
         this.main_god = god;
         this.altar = null;
+        this.sacredBook = null;
     }
+
 
     private Chest configAltarBlock(Block altar) {
         Block block = new Location(altar.getWorld(), altar.getX(), altar.getY(), altar.getZ()).getBlock();
@@ -86,5 +88,17 @@ public class Religion {
 
     public void removeTown(Town town) {
         this.towns.remove(town);
+    }
+
+    public SacredBook getSacredBook() {
+        return sacredBook;
+    }
+
+    public void setSacredBook(SacredBook sacredBook) {
+        this.sacredBook = sacredBook;
+    }
+
+    public void createSacredBook() {
+
     }
 }
---
+++ src/main/java/org/papiricoh/townyreligion/object/book/SacredBook.java
@@ -0,0 +1,15 @@
+package org.papiricoh.townyreligion.object.book;
+
+public class SacredBook {
+    private String title;
+    private String content;
+    private String god_name;
+
+    public SacredBook(String title, String content, String god_name) {
+        this.title = title;
+        this.content = content;
+        this.god_name = god_name;
+    }
+
+
+}
