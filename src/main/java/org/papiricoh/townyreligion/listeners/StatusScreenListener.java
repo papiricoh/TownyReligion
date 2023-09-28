16df0163158aca55fe2c7e83caf9a4c39857dac2
papiricoh <slendeman0200@gmail.com>
2023-09-28 12:09:17 +0200

    Added nationStatus to StatusScreenListener

--- src/main/java/org/papiricoh/townyreligion/listeners/StatusScreenListener.java
+++ src/main/java/org/papiricoh/townyreligion/listeners/StatusScreenListener.java
@@ -1,12 +1,19 @@
 package org.papiricoh.townyreligion.listeners;
 import com.palmergames.adventure.text.Component;
+import com.palmergames.bukkit.towny.event.statusscreen.NationStatusScreenEvent;
 import com.palmergames.bukkit.towny.event.statusscreen.TownStatusScreenEvent;
+import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Town;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.papiricoh.townyreligion.TownyReligion;
 import org.papiricoh.townyreligion.object.Religion;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatusScreenListener implements Listener {
 
@@ -19,6 +26,34 @@ public class StatusScreenListener implements Listener {
             }
         }
 
+    }
+
+    @EventHandler
+    public void onNationStatus(NationStatusScreenEvent event) {
+        Nation nation = event.getNation();
+        List<Town> towns = nation.getTowns();
+        Map<String, Integer> religions_towns = new HashMap<>();
+        for (Religion r: TownyReligion.religions) {
+            int number_of_towns = 0;
+            for (Town t: towns) {
+                if(r.containsTown(t)) {
+                    number_of_towns++;
+                }
+            }
+            if(number_of_towns != 0) {
+                religions_towns.put(r.getName(), number_of_towns);
+            }
+        }
+        if(religions_towns.size() < towns.size()) {
+            int to_add = towns.size() - religions_towns.size();
+            religions_towns.put("Atheism", to_add);
+        }
+        DecimalFormat df = new DecimalFormat("###.#");
+        String to_string = "";
+        for (String st: religions_towns.keySet()) {
+            to_string += st + ": " + ChatColor.DARK_RED + df.format((religions_towns.get(st) / religions_towns.size()) * 100) + "% " + ChatColor.WHITE;
+        }
+        event.getStatusScreen().addComponentOf("religion", Component.newline().append(Component.text(to_string)));
     }
 
 }
